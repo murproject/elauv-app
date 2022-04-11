7fd187c3ddd7b804ee0b9981631af4adb0a22981
Alex Zauzolkov <alex.zauzolkov@yandex.ru>
2022-04-11 10:40:22 +1000

    Indentation

--- src/blockly-wrapper/BlocklyStyle.js
+++ src/blockly-wrapper/BlocklyStyle.js
@@ -1,7 +1,7 @@
 import Blockly from 'blockly'
 
 var CustomRenderer = function (name) {
-    CustomRenderer.superClass_.constructor.call(this, name)
+  CustomRenderer.superClass_.constructor.call(this, name)
 }
 
 Blockly.utils.object.inherits(CustomRenderer, Blockly.thrasos.Renderer)
@@ -9,50 +9,50 @@ Blockly.utils.object.inherits(CustomRenderer, Blockly.thrasos.Renderer)
 Blockly.blockRendering.register('custom_renderer', CustomRenderer)
 
 var CustomConstantsProvider = function () {
-// Set up all of the constants from the base provider.
-CustomConstantsProvider.superClass_.constructor.call(this)
+  // Set up all of the constants from the base provider.
+  CustomConstantsProvider.superClass_.constructor.call(this)
 
-this.NOTCH_WIDTH = 15
-this.NOTCH_HEIGHT = 5
-this.NOTCH_OFFSET_LEFT = 18.8
+  this.NOTCH_WIDTH = 15
+  this.NOTCH_HEIGHT = 5
+  this.NOTCH_OFFSET_LEFT = 18.8
 
-// this.EXTERNAL_VALUE_INPUT_PADDING = 200
-// this.EMPTY_INLINE_INPUT_HEIGHT = 200
-// this.DUMMY_INPUT_SHADOW_MIN_HEIGHT = 200
-// this.STATEMENT_INPUT_NOTCH_OFFSET
+  // this.EXTERNAL_VALUE_INPUT_PADDING = 200
+  // this.EMPTY_INLINE_INPUT_HEIGHT = 200
+  // this.DUMMY_INPUT_SHADOW_MIN_HEIGHT = 200
+  // this.STATEMENT_INPUT_NOTCH_OFFSET
 
-this.CORNER_RADIUS = 0
-this.TAB_WIDTH = 5
-this.TAB_HEIGHT = 28 / 2
+  this.CORNER_RADIUS = 0
+  this.TAB_WIDTH = 5
+  this.TAB_HEIGHT = 28 / 2
 
-// this.EMPTY_INLINE_INPUT_PADDING = 200
-this.EXTERNAL_VALUE_INPUT_PADDING = 10
+  // this.EMPTY_INLINE_INPUT_PADDING = 200
+  this.EXTERNAL_VALUE_INPUT_PADDING = 10
 
-this.DUMMY_INPUT_MIN_HEIGHT = this.TAB_HEIGHT * 1.5
-this.DUMMY_INPUT_SHADOW_MIN_HEIGHT = this.TAB_HEIGHT
-this.EMPTY_INLINE_INPUT_HEIGHT = this.TAB_HEIGHT * 1.75
+  this.DUMMY_INPUT_MIN_HEIGHT = this.TAB_HEIGHT * 1.5
+  this.DUMMY_INPUT_SHADOW_MIN_HEIGHT = this.TAB_HEIGHT
+  this.EMPTY_INLINE_INPUT_HEIGHT = this.TAB_HEIGHT * 1.75
 
-this.TAB_OFFSET_FROM_TOP = 5
-// this.TAB_VERTICAL_OVERLAP = 800
+  this.TAB_OFFSET_FROM_TOP = 5
+  // this.TAB_VERTICAL_OVERLAP = 800
 
-this.RECT_PREV_NEXT = this.makeRectangularPreviousConn()
-this.RECT_INPUT_OUTPUT = this.makeRectangularInputConn()
+  this.RECT_PREV_NEXT = this.makeRectangularPreviousConn()
+  this.RECT_INPUT_OUTPUT = this.makeRectangularInputConn()
 }
 
 Blockly.utils.object.inherits(CustomConstantsProvider,
-Blockly.blockRendering.ConstantProvider)
+  Blockly.blockRendering.ConstantProvider)
 
 CustomRenderer.prototype.makeConstants_ = function () {
-return new CustomConstantsProvider()
+  return new CustomConstantsProvider()
 }
 
 CustomConstantsProvider.prototype.makeRectangularInputConn = function (e) {
-var width = this.TAB_WIDTH
-var height = this.TAB_HEIGHT
+  var width = this.TAB_WIDTH
+  var height = this.TAB_HEIGHT
 
-function makeMainPath (up) {
+  function makeMainPath(up) {
     return Blockly.utils.svgPaths.line(
-    [
+      [
         Blockly.utils.svgPaths.point(-width, -1 * up * (height / 2)),
         Blockly.utils.svgPaths.point(width, -1 * up * (height / 2))
 
@@ -63,62 +63,62 @@ function makeMainPath (up) {
 
         // Blockly.utils.svgPaths.point(-width, -1 * up * (height / 2)),
         // Blockly.utils.svgPaths.point(width, -1 * up * (height / 2))
-    ])
-}
+      ])
+  }
 
-var pathUp = makeMainPath(1)
-var pathDown = makeMainPath(-1)
+  var pathUp = makeMainPath(1)
+  var pathDown = makeMainPath(-1)
 
-return {
+  return {
     width: width,
     height: height,
     pathDown: pathDown,
     pathUp: pathUp
-}
+  }
 }
 
 CustomConstantsProvider.prototype.makeRectangularPreviousConn = function () {
-var width = this.NOTCH_WIDTH
-var height = this.NOTCH_HEIGHT
+  var width = this.NOTCH_WIDTH
+  var height = this.NOTCH_HEIGHT
 
-/**
- * Since previous and next connections share the same shape
- * you can define a function to generate the path for both.
- */
-function makeMainPath (dir) {
+  /**
+   * Since previous and next connections share the same shape
+   * you can define a function to generate the path for both.
+   */
+  function makeMainPath(dir) {
     return Blockly.utils.svgPaths.line(
-    [
+      [
         // Blockly.utils.svgPaths.point(0, height),
         // Blockly.utils.svgPaths.point(dir * width, 0),
         // Blockly.utils.svgPaths.point(0, -height)
         Blockly.utils.svgPaths.point(dir * width / 2, height),
         // Blockly.utils.svgPaths.point(dir * width, 0),
         Blockly.utils.svgPaths.point(dir * width / 2, -height)
-    ])
-}
-var pathLeft = makeMainPath(1)
-var pathRight = makeMainPath(-1)
+      ])
+  }
+  var pathLeft = makeMainPath(1)
+  var pathRight = makeMainPath(-1)
 
-return {
+  return {
     width: width,
     height: height,
     pathLeft: pathLeft,
     pathRight: pathRight
-}
+  }
 }
 
 CustomConstantsProvider.prototype.shapeFor = function (
-connection) {
-switch (connection.type) {
+  connection) {
+  switch (connection.type) {
     case Blockly.INPUT_VALUE:
     case Blockly.OUTPUT_VALUE:
-    return this.RECT_INPUT_OUTPUT
+      return this.RECT_INPUT_OUTPUT
     case Blockly.PREVIOUS_STATEMENT:
     case Blockly.NEXT_STATEMENT:
-    return this.RECT_PREV_NEXT
+      return this.RECT_PREV_NEXT
     default:
-    throw Error('Unknown connection type')
-}
+      throw Error('Unknown connection type')
+  }
 }
 
 // TODO: replace prompt dialogs - https://developers.google.com/blockly/reference/js/Blockly.dialog?hl=en
--- src/blockly-wrapper/BlocklyWrapper.js
+++ src/blockly-wrapper/BlocklyWrapper.js
@@ -10,48 +10,48 @@ import Blocks from './Blocks'
 import './BlocklyStyle'
 
 export default {
-    start: function () {
-        console.log("startin")
-        Blocks.init();
-        Blockly.setLocale(Ru)
-        var blocklyArea = document.getElementById('blocklyArea');
-        var blocklyDiv = document.getElementById('blocklyDiv');
-        console.log(Blockly);
-        console.log(blocklyDiv);
-        var demoWorkspace = Blockly.inject(blocklyDiv,
-            {
-                grid: {
-                    spacing: 26,
-                    length: 2,
-                    colour: '#ccc',
-                    snap: true
-                  },
-                  zoom: {
-                    controls: true,
-                    wheel: true,
-                    startScale: 1.0,
-                    maxScale: 2.0,
-                    minScale: 0.3,
-                    scaleSpeed: 1.5,
-                    pinch: true
-                  },
-                  // css: false,
-                  move: {
-                    drag: true,
-                    wheel: true
-                  },
-                  scrollbars: true,
-                  media: '/blockly-media/', // TODO!!!
-                  trashcan: true,
-                  // renderer: 'thrasos', // thrasos, zelos
-                  // renderer: 'zelos',
-                  renderer: 'custom_renderer',
-                  horizontalLayout: true,
-                  collapse: false,
-                  toolbox: MurToolbox,
-                // toolbox: document.getElementById('toolbox')
-            }
-        );
-        Blockly.svgResize(demoWorkspace);
-    }
+  start: function () {
+    console.log("startin")
+    Blocks.init();
+    Blockly.setLocale(Ru)
+    var blocklyArea = document.getElementById('blocklyArea');
+    var blocklyDiv = document.getElementById('blocklyDiv');
+    console.log(Blockly);
+    console.log(blocklyDiv);
+    var demoWorkspace = Blockly.inject(blocklyDiv,
+      {
+        grid: {
+          spacing: 26,
+          length: 2,
+          colour: '#ccc',
+          snap: true
+        },
+        zoom: {
+          controls: true,
+          wheel: true,
+          startScale: 1.0,
+          maxScale: 2.0,
+          minScale: 0.3,
+          scaleSpeed: 1.5,
+          pinch: true
+        },
+        // css: false,
+        move: {
+          drag: true,
+          wheel: true
+        },
+        scrollbars: true,
+        media: '/blockly-media/', // TODO!!!
+        trashcan: true,
+        // renderer: 'thrasos', // thrasos, zelos
+        // renderer: 'zelos',
+        renderer: 'custom_renderer',
+        horizontalLayout: true,
+        collapse: false,
+        toolbox: MurToolbox,
+        // toolbox: document.getElementById('toolbox')
+      }
+    );
+    Blockly.svgResize(demoWorkspace);
+  }
 }
