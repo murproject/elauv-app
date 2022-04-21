5e3015a920ab2284b4f64a1bfb3fd61e4efd6361
Alex Zauzolkov <alex.zauzolkov@yandex.ru>
2022-04-21 12:30:55 +1000

    Blockly: custom render progress

--- src/blockly-wrapper/BlocklyStyle.js
+++ src/blockly-wrapper/BlocklyStyle.js
@@ -15,6 +15,11 @@ var CustomConstantsProvider = function () {
   this.NOTCH_WIDTH = 15
   this.NOTCH_HEIGHT = 5
   this.NOTCH_OFFSET_LEFT = 18.8
+  // this.NOTCH_OFFSET_LEFT = 10
+
+  this.STATEMENT_INPUT_NOTCH_OFFSET = 19;
+  // this.STATEMENT_BOTTOM_SPACER = 0;
+  // this.STATEMENT_INPUT_PADDING_LEFT = 20;
 
   // this.EXTERNAL_VALUE_INPUT_PADDING = 200
   // this.EMPTY_INLINE_INPUT_HEIGHT = 200
@@ -23,20 +28,23 @@ var CustomConstantsProvider = function () {
 
   this.CORNER_RADIUS = 0
   this.TAB_WIDTH = 5
-  this.TAB_HEIGHT = 28 / 2
+  this.TAB_HEIGHT = 14
 
   // this.EMPTY_INLINE_INPUT_PADDING = 200
-  this.EXTERNAL_VALUE_INPUT_PADDING = 10
+  this.EXTERNAL_VALUE_INPUT_PADDING = 0
 
   this.DUMMY_INPUT_MIN_HEIGHT = this.TAB_HEIGHT * 1.5
   this.DUMMY_INPUT_SHADOW_MIN_HEIGHT = this.TAB_HEIGHT
   this.EMPTY_INLINE_INPUT_HEIGHT = this.TAB_HEIGHT * 1.75
 
   this.TAB_OFFSET_FROM_TOP = 5
+  this.BETWEEN_STATEMENT_PADDING_Y = 5
+
   // this.TAB_VERTICAL_OVERLAP = 800
 
-  this.RECT_PREV_NEXT = this.makeRectangularPreviousConn()
-  this.RECT_INPUT_OUTPUT = this.makeRectangularInputConn()
+  this.TRIG_PREV_NEXT = this.makeTriangularPreviousConn()
+  this.TRIG_INPUT_OUTPUT = this.makeTriangularInputConn()
+  this.RECT_INPUT_OUTPUT = this.makeRectInputConn()
 }
 
 Blockly.utils.object.inherits(CustomConstantsProvider,
@@ -46,15 +54,15 @@ CustomRenderer.prototype.makeConstants_ = function () {
   return new CustomConstantsProvider()
 }
 
-CustomConstantsProvider.prototype.makeRectangularInputConn = function (e) {
+CustomConstantsProvider.prototype.makeTriangularInputConn = function (e) {
   var width = this.TAB_WIDTH
   var height = this.TAB_HEIGHT
 
   function makeMainPath(up) {
     return Blockly.utils.svgPaths.line(
       [
-        Blockly.utils.svgPaths.point(-width, -1 * up * (height / 2)),
-        Blockly.utils.svgPaths.point(width, -1 * up * (height / 2))
+        Blockly.utils.svgPaths.point(-width, up * (height / 2)),
+        Blockly.utils.svgPaths.point(width, up * (height / 2))
 
         // Blockly.utils.svgPaths.point(-width / 2, 0),
         // Blockly.utils.svgPaths.point(-width, -1 * up * (height / 2)),
@@ -66,8 +74,8 @@ CustomConstantsProvider.prototype.makeRectangularInputConn = function (e) {
       ])
   }
 
-  var pathUp = makeMainPath(1)
-  var pathDown = makeMainPath(-1)
+  var pathUp = makeMainPath(-1)
+  var pathDown = makeMainPath(1)
 
   return {
     width: width,
@@ -77,7 +85,31 @@ CustomConstantsProvider.prototype.makeRectangularInputConn = function (e) {
   }
 }
 
-CustomConstantsProvider.prototype.makeRectangularPreviousConn = function () {
+CustomConstantsProvider.prototype.makeRectInputConn = function (e) {
+  var width = this.TAB_WIDTH
+  var height = this.TAB_HEIGHT
+
+  function makeMainPath(up) {
+    return Blockly.utils.svgPaths.line(
+      [
+        Blockly.utils.svgPaths.point(-width, up * (0)),
+        Blockly.utils.svgPaths.point(0, up * (height)),
+        Blockly.utils.svgPaths.point(width, up * (0))
+      ])
+  }
+
+  var pathUp = makeMainPath(-1)
+  var pathDown = makeMainPath(1)
+
+  return {
+    width: width,
+    height: height,
+    pathDown: pathDown,
+    pathUp: pathUp
+  }
+}
+
+CustomConstantsProvider.prototype.makeTriangularPreviousConn = function () {
   var width = this.NOTCH_WIDTH
   var height = this.NOTCH_HEIGHT
 
@@ -107,15 +139,55 @@ CustomConstantsProvider.prototype.makeRectangularPreviousConn = function () {
   }
 }
 
-CustomConstantsProvider.prototype.shapeFor = function (
-  connection) {
+CustomConstantsProvider.prototype.shapeFor = function (connection) {
+  // var checks = connection.getCheck();
+
+  let checks = connection.getCheck();
+  if (!checks && connection.targetConnection) {
+    checks = connection.targetConnection.getCheck();
+  }
+
+  let outputShape;
+
   switch (connection.type) {
     case Blockly.INPUT_VALUE:
     case Blockly.OUTPUT_VALUE:
-      return this.RECT_INPUT_OUTPUT
+
+      // console.log(connection.getSourceBlock().getOutputShape())
+
+      if (checks && checks.includes('Boolean')) {
+        return this.RECT_INPUT_OUTPUT
+      }
+      return this.TRIG_INPUT_OUTPUT
+
+      // TODO: make outputShape, like in zelos renderer
+      // outputShape = connection.getSourceBlock().getOutputShape();
+      // if (outputShape !== null) {
+      //   switch (outputShape) {
+      //     case this.SHAPES.HEXAGONAL:
+      //       return this.HEXAGONAL;
+      //     case this.SHAPES.ROUND:
+      //       return this.ROUNDED;
+      //     case this.SHAPES.SQUARE:
+      //       return this.SQUARED;
+      //   }
+      // }
+
+      // if (checks && checks.indexOf('Boolean') !== -1) {
+      //   return this.HEXAGONAL;
+      // }
+      // if (checks && checks.indexOf('Number') !== -1) {
+      //   return this.ROUNDED;
+      // }
+      // if (checks && checks.indexOf('String') !== -1) {
+      //   return this.ROUNDED;
+      // }
+
+      // return (this.ROUNDED);
+
     case Blockly.PREVIOUS_STATEMENT:
     case Blockly.NEXT_STATEMENT:
-      return this.RECT_PREV_NEXT
+      return this.TRIG_PREV_NEXT
     default:
       throw Error('Unknown connection type')
   }
