57ceead9fb4b07f51df34cf44e6a0dd28506d7fa
Alex Zauzolkov <alex.zauzolkov@yandex.ru>
2022-04-26 14:26:52 +1000

    Panel button: change to div

--- src/panels/Panel.js
+++ src/panels/Panel.js
@@ -18,7 +18,8 @@ export default class Panel {
     this.container.innerHTML = this.html;
     document.querySelector("#panel-wrapper").appendChild(this.container);
 
-    this.panelButton = document.createElement("button");
+    this.panelButton = document.createElement("div");
+    this.panelButton.classList.add("panel-button");
     this.panelButton.onclick = () => document.app.panelSelect(this.name);
     this.panelButton.innerText = this.name;
     document.querySelector("#buttons-main").appendChild(this.panelButton);
@@ -39,8 +40,10 @@ export default class Panel {
 
     if (this.active) {
       this.container.classList.add('active');
+      this.panelButton.classList.add('active');
     } else {
       this.container.classList.remove('active');
+      this.panelButton.classList.remove('active');
     }
 
     this.onActiveChanged();
