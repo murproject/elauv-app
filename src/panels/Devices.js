7299b5c09221c764a63002504035a8df309ff475
Alex Zauzolkov <alex.zauzolkov@yandex.ru>
2022-04-27 14:41:13 +1000

    Devices panel: drafts

--- src/panels/Devices.js
+++ src/panels/Devices.js
@@ -5,18 +5,39 @@ export default class Devices extends Panel {
   begin() {
     this.html = /*html*/`
       <div>
-        Devices list:
-        <ul>
-            <li>a</li>
-            <li>b</li>
-        </ul>
+        Status - <span id="connStatus"></span>
+      </div>
+      <br>
+      <div>
+        <div class="panel-button push-button" id="connScanDevices">Scan Devices</div>
+        <div class="panel-button push-button" id="connScanCode">Scan Code</div>
+      </div>
+      <br>
+      <div>
+        Available devices:
       </div>
     `
   }
 
 
   init() {
+    this.statusEl = this.q("#connStatus");
+    this.statusEl.innerText = "waiting…";
 
+    this.q("#connScanDevices").onclick = () => this.scanDevices();
+    this.q("#connScanCode").onclick = () => this.scanCode();
   }
+
+
+  scanDevices() {}
+
+
+  scanCode() {}
+
+
+  updateDevicesList() {}
+
+
+  updateConectedDevice() {}
 
 }
