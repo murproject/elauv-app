0b4c65c5043f17b1464e208ecf1e846e359bdc1f
Alex Zauzolkov <alex.zauzolkov@yandex.ru>
2022-06-27 16:26:30 +1000

    Projects: panel work in progress

--- src/panels/Projects.js
+++ src/panels/Projects.js
@@ -34,26 +34,20 @@ let example = [
   },
 ];
 
+function dateString(date) {
+  return dayjs(date).format('D MMMM<br>HH:mm');
+}
+
+
+function dateRelative(date) {
+  return dayjs(date).format('dddd<br>') + dayjs(date).fromNow();
+}
+
 export default class Projects extends Panel {
 
   begin() {
     this.html = /*html*/`
-      <div id="projects-head-buttons" class="row font-thin">
-        <div class="push-button button-vertical" id="">
-          ${Icon('content-save')}
-          <span class="caption">Сохранить</span>
-        </div>
-
-        <div class="push-button button-vertical" id="">
-          ${Icon('import')}
-          <span class="caption">Импорт из файла</span>
-        </div>
-
-        <div class="push-button button-vertical" id="">
-          ${Icon('export')}
-          <span class="caption">Экспорт в файл</span>
-        </div>
-      </div>
+      <div id="projects-head-buttons" class="row font-thin"></div>
 
       <div class="list-wrapper row">
         <div id="projects-list" class="width-fill"></div>
@@ -64,71 +58,123 @@ export default class Projects extends Panel {
   init() {
     this.setIcon('folder-open');
     this.projectsListEl = this.q('#projects-list');
-    this.headButtonsEl = this.q('#projects-head-buttons')
-
-    new Button({
-      name: 'save',
-      text: 'Сохранить',
-      type: 'push-button',
-      action: () => {},
-      icon: 'content-save',
-    }).inject(this.headButtonsEl);
-
+    this.makeButtons();
     this.projectsList = [];
     this.loadProjectsList();
     this.displayProjects();
   }
 
+  makeButtons() {
+    this.headButtonsEl = this.q('#projects-head-buttons')
+
+    new Button({
+      name: 'save',
+      text: 'Сохранить',
+      action: () => {},
+      // classes: 'button-vertical',
+      icon: 'content-save',
+    }).inject(this.headButtonsEl);
+
+    new Button({
+      name: 'import',
+      text: 'Импорт из файла',
+      action: () => {},
+      // classes: 'button-vertical',
+      icon: 'import',
+    }).inject(this.headButtonsEl);
+
+    new Button({
+      name: 'export',
+      text: 'Экспорт в файл',
+      action: () => {},
+      // classes: 'button-vertical',
+      icon: 'export',
+    }).inject(this.headButtonsEl);
+  }
+
   loadProjectsList() {
     this.projectsList = example; // TODO
+    // for (let i = 0; i < 100; i++) {
+    //   let name = '';
+    //   const iter = Math.round(Math.random() * 6) + 1
+    //   for (let j = 0; j < iter; j++) {
+    //     name += Math.random().toString(36).slice(2, Math.random() * 8 + 3) + ' ';
+    //   }
+    //   const item = {
+    //     name: name,
+    //     date: new Date(+new Date - Math.round(Math.random() * 10000000000)),
+    //     data: {},
+    //   };
+    //   this.projectsList.push(item);
+    // }
     this.projectsList = this.projectsList.sort((a, b) => b.date - a.date);
   }
 
-  displayProjects() {
+  makeProjectItem(item, index) {
+    const isEditing = index == 1; // TODO //
+
+    let itemEl = document.createElement("div");
+    itemEl.classList.add("list-item");
+
+    if (isEditing) {
+      itemEl.classList.add("active");
+    }
+
+    itemEl.innerHTML = `
+      <!-- ${Icon('file-table-outline', 'opacity-75')} -->
+
+      ${Icon(isEditing ? 'puzzle-edit' : 'puzzle', isEditing ? 'cyan opacity-75' : 'opacity-50')}
+
+      <div class="list-item-title">
+        ${item.name}
+      </div>
+
+      <div class="dateRelative">
+        <span class="opacity-50">${dateRelative(item.date)}</span>
+      </div>
+
+      <div class="dateAbsolute">
+        <span>${dateString(item.date)}</span>
+      </div>
+
+      <div class="icon-button">
+        ${Icon('trash-can-outline', 'red opacity-75')}
+      </div>
+    `;
+
+    itemEl.onclick = () => this.projectSelect(index);
+
+    return itemEl;
+  }
+
+  makeExamplesItem() {
+    let itemEl = document.createElement("div");
+    itemEl.classList.add("list-item");
+
+    itemEl.innerHTML = `
+      ${Icon('folder', 'dark opacity-50')}
+      <div class="list-item-title">Примеры</div>
+
+      <div class="dateAbsolute">
+        <span><br><br></span>
+      </div>
+
+      <div class="dateRelative">
+        <span class="opacity-50">Встроенные примеры программ</span>
+      </div>
+    `;
+
+    itemEl.onclick = () => this.displayProjects(true); // TODO //
+    return itemEl;
+  }
+
+  displayProjects(showExamples = false) {
     this.projectsListEl.innerHtml = '';
-
-    function dateString(date) {
-      return dayjs(date).format('D MMMM<br>HH:mm');
-    }
-
-    function dateRelative(date) {
-      return dayjs(date).format('dddd<br>') + dayjs(date).fromNow();
-    }
+    this.projectsListEl.innerText = '';
+    this.projectsListEl.appendChild(this.makeExamplesItem());
 
     this.projectsList.forEach((item, index) => {
-      const isEditing = index == 1; // TODO //
-
-      let itemEl = document.createElement("div");
-      itemEl.classList.add("list-item");
-
-      if (isEditing) {
-        itemEl.classList.add("active");
-      }
-
-      itemEl.innerHTML = `
-        <!-- ${Icon('file-table-outline', 'opacity-75')} -->
-
-        ${Icon(isEditing ? 'puzzle-edit' : 'puzzle', isEditing ? 'cyan opacity-75' : 'opacity-50')}
-
-        <div class="list-item-title">
-          ${item.name}
-        </div>
-
-        <div class="dateRelative">
-          <span class="opacity-50">${dateRelative(item.date)}</span>
-        </div>
-
-        <div class="dateAbsolute">
-          <span>${dateString(item.date)}</span>
-        </div>
-
-        <div class="icon-button">
-          ${Icon('trash-can-outline', 'red opacity-75')}
-        </div>
-      `;
-
-      itemEl.onclick = () => this.projectSelect(index);
-      this.projectsListEl.appendChild(itemEl);
+      this.projectsListEl.appendChild(this.makeProjectItem(item, index));
     });
   }
 
