af9dac177e95d2b957a58ad08687c1cfc98ac406
Alex Zauzolkov <alex.zauzolkov@yandex.ru>
2022-08-18 13:45:30 +1000

    Add webpack-auto-inject-version plugin

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,24 @@
+const WebpackAutoInject = require('webpack-auto-inject-version');
+
 module.exports = {
   publicPath: '',
   pluginOptions: {
     cordovaPath: 'src-cordova',
+  },
+  configureWebpack: {
+    plugins: [
+      new WebpackAutoInject({
+        components: {
+          AutoIncreaseVersion: false,
+          InjectAsComment: false,
+          InjectByTag: true,
+        },
+        componentsOptions: {
+          InjectByTag: {
+            dateFormat: 'yyyy-mm-dd',
+          },
+        },
+      }),
+    ],
   },
 };
